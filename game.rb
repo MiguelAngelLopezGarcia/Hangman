3f316849c21fc037ddc06c3262b2635c1fa6a6be
Miguel Angel Lopez <malg1992@gmail.com>
2020-09-30 10:59:19 +0200

    Add dictionary file complete and game file with only the rules

---
+++ game.rb
@@ -0,0 +1,6 @@
+#Game about the hangman where the computer choooses a word between 5 and 12 characters long randomly
+#Display draw and numbers of letters of the word selected
+#Whenever the user guess a letter (case insensitive), replace if correct or note down if incorrect. Modify round counter and draw
+#At the beggining of each turn the user can save the game
+#First load of the programm check if there is a saved game and executes it if so
+
